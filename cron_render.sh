7affd7888309eeca365b3fe7bf441cf4fe01459b
Boris-Barboris <ismailsiege@gmail.com>
2021-05-27 10:31:44 +0300

    account for simulator_id in kill_records table

--- cron_render.sh
+++ cron_render.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JSON_DATA="$(mysql -B dsubs_prod -e 'select * from kill_records where created_at < UTC_TIMESTAMP - INTERVAL 30 minute order by id desc limit 250;' | csvjson)"
+JSON_DATA="$(mysql -B dsubs_prod -e 'select * from kill_records where created_at < UTC_TIMESTAMP - INTERVAL 30 minute and simulator_id like "main%" order by id desc limit 250;' | csvjson)"
 echo -n '<link href="https://unpkg.com/tabulator-tables@4.4.1/dist/css/tabulator.min.css" rel="stylesheet">
 <script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.4.1/dist/js/tabulator.min.js"></script>
 <div id="kills_table"></div>
@@ -10,8 +10,8 @@ echo '    var table = new Tabulator("#kills_table", {
         data: kill_records,
         layout:"fitColumns",
         columns:[
-            {title:"Id", field:"id"},
             {title:"Timestamp", field:"created_at"},
+            {title:"simulator_id", field:"simulator_id"},
             {title:"Shooter", field:"shooter_captain_name"},
             {title:"Shooter hull", field:"shooter_hull_name"},
             {title:"Victim", field:"dead_captain_name"},
@@ -20,5 +20,5 @@ echo '    var table = new Tabulator("#kills_table", {
             {title:"Weapon travel distance", field:"weapon_travelled"}
         ]
     });
-    table.setSort("id", "desc");
+    table.setSort("created_at", "desc");
 </script>' >> index.html
